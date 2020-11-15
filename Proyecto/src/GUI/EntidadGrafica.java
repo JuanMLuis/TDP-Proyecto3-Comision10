95e22c9d4a6509a558ca241c3dbafeea64198b1d
JuanMLuis <juanmaluis98@gmail.com>
2020-11-15 15:42:55 -0300

    Entidades graficas

--- Proyecto/src/GUI/EntidadGrafica.java
+++ Proyecto/src/GUI/EntidadGrafica.java
@@ -1,5 +1,16 @@
 package GUI;
 
-public class EntidadGrafica {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
+public abstract class EntidadGrafica {
+		
+		protected JLabel miGrafico;
+	
+	public abstract void AvtualizarPosicion();
+	
+	public void setGrafico(ImageIcon i) {
+		miGrafico.setIcon(i);
+		
+	}
 }
