6c52593508a0df9c5f6ba225eac1be905460d2b7
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2020-11-17 14:29:21 -0300

    Update Enemigo.java

--- Proyecto/src/LogicaAbstracta/Enemigo.java
+++ Proyecto/src/LogicaAbstracta/Enemigo.java
@@ -3,6 +3,11 @@ package LogicaAbstracta;
 public abstract class Enemigo extends Personaje  {
 	
 	
+	public Enemigo() { 
+		vida = 100;
+	}
+	
+	
 	public void disparar() {
 		// TODO Auto-generated method stub
 		
