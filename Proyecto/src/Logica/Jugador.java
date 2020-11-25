d4fa96a87d55cfcc3395e1e0cee880984e313389
LeooRodriguez <leoorodriguez179@gmail.com>
2020-11-25 10:12:14 -0300

    LR:Estado Auxiliar

--- Proyecto/src/Logica/Jugador.java
+++ Proyecto/src/Logica/Jugador.java
@@ -37,7 +37,6 @@ public class Jugador extends Personaje  {
 		if(cooldown>=0)			
 			cooldown--;
 		estadoActual.movimiento(direccionActual,velocidad);
-		estadoActual.movimiento('x', velocidad);//Para que no siga moviendose
 	}
 	
 	
