1d7c5e8f31a1fdd613d116accbc56d5a24c38841
JuanMLuis <juanmaluis98@gmail.com>
2020-12-04 16:05:22 -0300

    Update EfectoTemporal.java

--- Proyecto/src/LogicaAbstracta/EfectoTemporal.java
+++ Proyecto/src/LogicaAbstracta/EfectoTemporal.java
@@ -2,13 +2,25 @@ package LogicaAbstracta;
 
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public abstract class EfectoTemporal extends Premio{ //cada uno de los metodos separarlos en 2 clases distintas
 	protected Timer timer;
+	protected int fin=100;
 
 	public void turno() {
+	if(posY<=430) {
 		for(int i = 0; i < velocidad; i++)
 			moverse('s');
-			colision();
+	}else
+		fin--;
+	
+	if(fin==0)
+		eliminar();
+	
+	
+	
+		colision();
 	}
+
 }
